8a66250bbb4ca03a78bad3d4a5b717a37643da7e
BhargavasRamus <29677592+BhargavasRamus@users.noreply.github.com>
2018-08-18 22:48:52 +0530

    Update 1_c.py

--- 1_c.py
+++ 1_c.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Number of training samples
 N = 10
-# Generate equispaced floats in the interval [0, 2π]
+# Generate equispaced floats in the interval [0, 2pi]
 x = np.linspace(0, 2*np.pi, N)
 # Generate noise
 mean = 0
@@ -13,18 +13,22 @@ y = np.sin(x)
 # Add noise
 y += np.random.normal(mean, std, N)
 
-xT=x.reshape((-1,1))
+x=x.reshape((-1,1))
+# transpose of x
 print "enter the highest degree of the poynomial="
 m=input()
 m= int(m)
+# degree of polynomial
 print "enter the value of Lagrangian multiplier="
 l=input()
 l=float(l)
+# Lagrangian multiplier
 X=np.full(np.shape(x),1)
 for i in range(1,m,1)
     X_=x**i
     X=np.column_stack(X,X_)
 print X
+# basis function
 
 W = np.zeros((N,N))
 X_inv=np.linalg.inv(np.matmul(X.T,X)+l*np.identity(m))
