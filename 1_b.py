8142eca0a26063c7217281d68d925ff2e8764ebb
BhargavasRamus <29677592+BhargavasRamus@users.noreply.github.com>
2018-08-18 23:07:57 +0530

    Update 1_b.py

--- 1_b.py
+++ 1_b.py
@@ -15,8 +15,6 @@ y += np.random.normal(mean, std, N)
 
 x=x.reshape((-1,1))
 # transpose of x
-x0=np.full(np.shape(x),1)
-# 1D matrix with 1as elements
 print "enter the highest degree of the poynomial="
 m=input()
 m= int(m)
@@ -24,13 +22,13 @@ m= int(m)
 X=np.full(np.shape(x),1)
 for i in range(1,m,1)
     X_=x**i
-    X=np.column_stack(X,X_)
+    X=np.column_stack((X,X_))
 print X
 # basis function
 W = np.zeros((N,N))
 W = np.matmul(np.linalg.pinv(X),y)
 print "W="
-print W
+print W)
 Y=np.matmul(X,W)
 # y'=xw
 plt.plot(x,y,'*',X,Y,'-')
