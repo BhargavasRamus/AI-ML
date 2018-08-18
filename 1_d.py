4d2d818bd0cdcf3fa6a92a9a15f9eef4c3b22bd7
BhargavasRamus <29677592+BhargavasRamus@users.noreply.github.com>
2018-08-18 15:21:39 +0530

    Create 1_d.py

---
+++ 1_d.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Number of training samples
+N = 10
+# Generate equispaced floats in the interval [0, 2π]
+x = np.linspace(0, 2*np.pi, N)
+# Generate noise
+mean = 0
+std = 0.05
+# Generate some numbers from the sine function
+y = np.sin(x)
+# Add noise
+y += np.random.normal(mean, std, N)
+xT=x.reshape((-1,1))
+x0=np.full(np.shape(x),1)
+X=np.column_stack(x0,x)
+W = np.zeros((N,N))
+W = np.matmul(np.linalg.pinv(X),y)
+print "W="
+print W
+Y=np.matmul(X,W)
+plt.plot(x,y,'*',X,Y,'-')
+plt.show()
+
+error=Y-y
+print "variance in y:"
+print error
