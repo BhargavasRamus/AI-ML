468b3e65c8fa9a92285c699560b6e40a908be365
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2018-08-28 11:15:08 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas
+
+X_0=pandas.read_csv("X.csv",header=None)
+Y_0=pandas.read_csv("Y.csv",header=None)
+X=np.transpose(np.array(X_0, dtype=np.float64))
+Y=np.array(Y_0, dtype=np.float64)
+X_test = np.array([[1,1],[1,-1],[-1,1],[-1,-1]])
+
+X_1=X
+X_2=X
+N=0
+M=0
+for i in range(0,len(Y)):
+    if Y[i]==-1.0:
+        
+        X_1=np.delete(X_1,i-N,axis=0)
+        N=N+1
+    if Y[i]==1.0:
+        
+        X_2=np.delete(X_2,i-N,axis=0)
+        M=M+1
+V_X1=np.var(X_1,axis=0)
+V_X2=np.var(X_2,axis=0)
+M_X1=np.mean(X_1,axis=0)
+M_X2=np.mean(X_2,axis=0)
+
+for i in range(0,len(X_test)):
+    pr_1 = (len(X_1)/(len(Y)*np.sqrt(2*np.pi*V_X1[0,1]*V_X1[0,0])))*np.exp(-((X_test[i][0]-M_X1[0,0])**2)/(2*V_X1[0,0]))*np.exp(-((X_test[i][1]-M_X1[0,1])**2)/(2*V_X1[0,1]))
+    pr_2 = (len(X_2)/(len(Y)*np.sqrt(2*np.pi*V_X2[0,1]*V_X2[0,0])))*np.exp(-((X_test[i][0]-M_X2[0,0])**2)/(2*V_X2[0,0]))*np.exp(-((X_test[i][1]-M_X2[0,1])**2)/(2*V_X2[0,1]))
+    if pr_1>pr_2:
+        print(X_test[i],"corresponds to Y=1")
+    else:
+        print(X_test[i],"corresponds to Y=-1")
---
+++ b.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas
+
+def euclidean_dist(X_test,X):
+	dist = np.zeros((1000,1))
+	for j in range(0,1000):
+		dist[j] = np.sqrt(((X_test[0]-X[j][0])**2)+((X_test[1]-X[j][1])**2))
+	dist = np.reshape(dist,1000)
+	return dist
+
+def bubbleSort(array):
+    for passnum in range(len(array)-1,0,-1):
+        for i in range(passnum):
+            if array[i][0]>array[i+1][0]:
+                temp = array[i][0]
+                array[i][0] = array[i+1][0]
+                array[i+1][0] = temp
+                temp1 = array[i][1]
+                array[i][1] = array[i+1][1]
+                array[i+1][1] = temp1
+    return array
+
+X_0=pandas.read_csv("X.csv",header=None)
+Y_0=pandas.read_csv("Y.csv",header=None)
+X=np.transpose(np.array(X_0, dtype=np.float64))
+Y=np.array(Y_0, dtype=np.float64)
+
+X_test = np.array([[1,1],[1,-1],[-1,1],[-1,-1]])
+
+print("enter the value of K \n")
+K = input()
+K = int(K)
+
+for i in range(0,len(X_test)):
+	dist = euclidean_dist(X_test[i],X)
+	dist_Y = np.transpose(np.array((dist,Y.reshape(1000,))))
+	sort_1 = bubbleSort(dist_Y)
+	K_elems = sort_1[:K]
+	K_elems = K_elems.ravel()
+	expectation = np.sum(K_elems[-K:])
+	if expectation>0 :
+		print(X_test[i],"corresponds to Y=1 \n")
+	else:
+		print(X_test[i],"corresponds to Y=-1 \n")
